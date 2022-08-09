1cfd5697d1638ea65c141ff6bd2920a89c7c3442
lerka22eklerka <94803605+lerka22eklerka@users.noreply.github.com>
2022-08-09 15:52:23 +0300

    in_progress

---
+++ src/js/02-timer.js
@@ -0,0 +1,67 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const inputRef = document.querySelector('#datetime-picker');
+const btnStart = document.querySelector('[data-start]');
+const dataDays = document.querySelector('[data-days]');
+const dataHours = document.querySelector('[data-hours]');
+const dataMinutes = document.querySelector('[data-minutes]');
+const dataSeconds = document.querySelector('[data-seconds]');
+
+btnStart.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      console.log(selectedDates[0]);
+      console.log(options.defaultDate);
+      if (selectedDates[0] < options.defaultDate) {
+          alert("Please choose a date in the future");
+          btnStart.disabled = true;
+      }
+      btnStart.disabled = false;
+   
+  },
+};
+flatpickr('#datetime-picker', options);
+
+btnStart.addEventListener('click', onStartTimer);
+let intervalId;
+function onStartTimer() {
+    intervalId = setInterval(() => {
+    
+}, 1000)
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+function addLeadingZero(value) {
+    padStart() {
+
+    }
+}
+
